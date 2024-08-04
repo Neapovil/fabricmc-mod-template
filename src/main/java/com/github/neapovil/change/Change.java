aff8e4a8cbdce6917137a75ff0e39d914444a2b7
William <nearata@protonmail.com>
2024-08-04 15:24:19 +0200

    add access widener
    * formatting

--- src/main/java/com/github/neapovil/change/Change.java
+++ src/main/java/com/github/neapovil/change/Change.java
@@ -1,9 +1,8 @@
 package com.github.neapovil.change;
 
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.fabricmc.api.ClientModInitializer;
 
 public final class Change implements ClientModInitializer
 {
